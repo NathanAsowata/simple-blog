79efe68203de1a8bf42d0e0ff7914e1003771abb
Nathan Asowata <nathanasowata@gmail.com>
2023-01-02 20:43:05 +0100

    Remove boilerplate code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
-import '../styles/globals.css'
+import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return(
+    <>
+      <Head>
+        <title>Simple Blog</title>
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
