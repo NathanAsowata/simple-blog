d386eeb7ee6884413e90f4ff9d82c13fb6432d81
Nathan Asowata <nathanasowata@gmail.com>
2023-01-05 08:13:06 +0100

    feat: Add blog feed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,46 @@
+import { useState, useEffect} from "react"
+import { collection, getDocs} from "firebase/firestore"
+import db from "../backend/firebase"
 
 
-const Home = () => {
-  return (
-    <div>Home</div>
-  )
+interface Document {
+  content: String,
+  title: String
 }
 
-export default Home
+const Home = () => {
+
+  const [documents, setDocuments] = useState<Document[]>([])
+  
+  useEffect(() => {
+
+    const getPosts = async () => {
+      
+      const fetchDocuments: Document[] = [];
+      
+      const data = await getDocs(collection(db, "posts"))
+      
+      data.docs.map(doc => {
+        fetchDocuments.push(doc.data() as Document)
+        
+      })
+      
+      setDocuments(fetchDocuments)
+    }
+    
+    getPosts()
+    
+  }, [])
+
+  return(
+    <ul>
+      {documents.map((doc, id) => {
+        return <li key={id}>
+                  <h4>{doc.title}</h4>
+                  <p>{doc.content}</p>
+                </li>
+      })}
+    </ul>
+  )
+}
+export default Home;
