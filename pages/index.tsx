bd1e92d94b5f87124448fd92142442b179f0652d
Nathan Asowata <nathanasowata@gmail.com>
2023-01-05 08:35:05 +0100

    refactor: Added comments to the getPosts function

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,29 +13,37 @@ const Home = () => {
 
   const [documents, setDocuments] = useState<Document[]>([])
   
+  // The useEffect hook gets all posts every time the page is reloaded
   useEffect(() => {
 
+    // This function gets all posts from the database
     const getPosts = async () => {
       
+      // This is an empty array to store that posts coming from the database
       const fetchDocuments: Document[] = [];
       
+      // This functions gets data from the database and stores it in a variable
       const data = await getDocs(collection(db, "posts"))
       
+      // The data is then  mapped to extract the blog posts and insert them in to an array
       data.docs.map(doc => {
-        fetchDocuments.push(doc.data() as Document)
-        
+        fetchDocuments.push(doc.data() as Document)   
       })
       
+      // This sets the fetchDocuments as the new value for the document state
       setDocuments(fetchDocuments)
     }
     
+    // The getPost function is called
     getPosts()
     
   }, [])
 
   return(
     <ul className={styles.blogFeed}>
+      
       {documents.map((doc, id) => {
+        
         return <li key={id} className={styles.post}>
                   <button>Delete</button>
                   <h4>{doc.title}</h4>
